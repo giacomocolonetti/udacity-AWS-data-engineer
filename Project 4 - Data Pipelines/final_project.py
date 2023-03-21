6acfd56a90a26abae6cecb6cc5048892ba74a1e4
Giacomo Colonetti <giacomo@giacomocolonetti.com>
2023-03-21 10:29:50 -0300

    style: improve DAG definition

--- Project 4 - Data Pipelines/final_project.py
+++ Project 4 - Data Pipelines/final_project.py
@@ -93,21 +93,15 @@ def final_project():
 
     end_operator = DummyOperator(task_id='End_execution')
 
-    start_operator >> stage_events_to_redshift
-    start_operator >> stage_songs_to_redshift
+    start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
 
-    stage_events_to_redshift >> load_songplays_table
-    stage_songs_to_redshift >> load_songplays_table
+    load_songplays_table << [stage_songs_to_redshift, stage_events_to_redshift]
 
-    load_songplays_table >> load_song_dimension_table
-    load_songplays_table >> load_user_dimension_table
-    load_songplays_table >> load_artist_dimension_table
-    load_songplays_table >> load_time_dimension_table
+    load_songplays_table >> [load_song_dimension_table, load_user_dimension_table,
+                             load_artist_dimension_table, load_time_dimension_table]
 
-    load_song_dimension_table >> run_quality_checks
-    load_user_dimension_table >> run_quality_checks
-    load_artist_dimension_table >> run_quality_checks
-    load_time_dimension_table >> run_quality_checks
+    run_quality_checks << [load_song_dimension_table, load_user_dimension_table,
+                           load_artist_dimension_table, load_time_dimension_table]
 
     run_quality_checks >> end_operator
 
